bd7ea05ddae6fdf2f4bef08774691b5895991438
Ryosuke Yamano <ryosuke.y.97@gmail.com>
2018-01-06 20:38:24 +0900

    make: todo作成フォームコンポーネントを作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule,
